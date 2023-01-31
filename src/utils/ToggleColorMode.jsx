2b89a22a5a2312ea2bdb4238194795e6e1fd01b1
zchan012 <zchan012@e.ntu.edu.sg>
2023-01-31 22:40:43 +0800

    updated default color theme

--- src/utils/ToggleColorMode.jsx
+++ src/utils/ToggleColorMode.jsx
@@ -1,26 +1,32 @@
-import React, { useState, useMemo, createContext } from 'react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+/* eslint-disable comma-dangle */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable react/jsx-no-constructed-context-values */
+/* eslint-disable quotes */
+import React, { useState, useMemo, createContext } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 export const ColorModeContext = createContext();
 
 function ToggleColorMode({ children }) {
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("dark");
 
   const toggleColorMode = () => {
-    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+    setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
   };
 
-  const theme = useMemo(() => createTheme({
-    palette: {
-      mode,
-    },
-  }), [mode]);
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
 
   return (
     <ColorModeContext.Provider value={{ mode, setMode, toggleColorMode }}>
-      <ThemeProvider theme={theme}>
-        {children}
-      </ThemeProvider>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </ColorModeContext.Provider>
   );
 }
